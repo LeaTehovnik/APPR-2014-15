2117a49e52f41bf17aec3288cb9c7246ff2502e7
Lea Tehovnik <lea.tehovnik@gmail.com>
2014-12-03 09:11:17 +0100

    Popravila grafe.

--- lib/graf.R
+++ lib/graf.R
@@ -1,7 +1,13 @@
 pdf("slike/grafi.pdf",paper="a4r")
-#Graf, ki prikazuje delež držav, iz katerih so rekorderji
+pdf.options(encoding='ISOLatin2.enc')
+#Graf, ki prikazuje delež držav, iz katerih so maratonci
 
 table(maraton$Nationality)
-pie(table(maraton$Nationality))
+pie(table(maraton$Nationality), main="Državljanstva maratoncev")
+
+#graf razmerja med spolom
+
+table(maraton$Spol)
+pie(table(maraton$Spol), main="Razmerje med spoloma")
 
 dev.off()
