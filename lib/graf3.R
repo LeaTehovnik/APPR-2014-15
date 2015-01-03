4b55441a784b4eaf83f8e4118907a380d7b4d384
Lea Tehovnik <lea.tehovnik@gmail.com>
2015-01-03 14:45:33 +0100

    Dodala še en graf za pomoč pri zemljevidu

---
+++ lib/graf3.R
@@ -0,0 +1,18 @@
+#rada bi naredila graf, ki bo prikazoval ,kolikokrat je bil na nekem maratonu (v državi) 
+#postavljen rekord. Potem pa bom te podatke še pokazala na zemljevidu
+
+  
+pdf("slike/graf3.pdf")
+pdf.options(encoding='ISOLatin2.enc')
+
+
+kraj <- table(maraton$Kraj)
+kraj <- kraj[order(kraj, decreasing=TRUE)]
+druge <- kraj < 2 
+kraj <- c(kraj[!druge], "Druge" = sum(kraj[druge]))
+pie(kraj, main="Število postavljenih rekordov na posameznem maratonu", clockwise = TRUE, cex = 1)
+
+
+
+
+dev.off()
