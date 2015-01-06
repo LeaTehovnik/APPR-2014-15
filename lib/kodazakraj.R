ff0fd71aeee9b273d8cf1aebe73d019f0f09a9c1
Lea Tehovnik <TehovnikL12@R309-04.std.fmf.uni-lj.si>
2015-01-06 11:53:50 +0100

    Težave..

--- lib/kodazakraj.R
+++ lib/kodazakraj.R
@@ -1,4 +1,6 @@
 #Rada bi kraj oz. ime maratona povezala z državo
+#To bom potem prekopirala v vizualizacijo.r
+
 
 # Kar se tiče države maratona, jo lahko ponekod dobiš iz
 # samega imena kraja (kjer je oblike Kraj, Država), 
@@ -10,6 +12,20 @@
 
 imenadrzav <- maraton$Drzavljanstvo
 imenadrzav <- imenadrzav[!duplicated(imenadrzav)]
-kraj <- table(maraton$Kraj)
+kraj <- table(maraton$Kraj)  #kaj pa bre table?
 kraj <- kraj[order(kraj, decreasing=TRUE)]
-kraj <- kraj[kraj %in% imenadrzav]
+kraj <- kraj[kraj %in% imenadrzav] #ali je kraj v imenu držav
+
+
+#če dobim nek vektor
+# x = nam pove ali se država ujema z državljanstvom, y ,če se ne
+tujec <-  len(x) > len(y)...! %in% imenadrzav
+drzavljan <- len(y) > len(x)
+enako <- len(x) = len(y)
+#to bo imenska spremenljivka
+
+#točke na zemlejvidu
+text(coordinates(svet), 
+     labels = as.character(imenadrzav), cex = 0.7)
+barve <- rainbow(49)
+plot(svet, col = barve)
