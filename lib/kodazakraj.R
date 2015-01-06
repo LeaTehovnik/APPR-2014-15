1a21ad1aad5083830e92853d65eb7e13c218d034
Lea Tehovnik <TehovnikL12@R310-05.std.fmf.uni-lj.si>
2015-01-06 14:53:45 +0100

    Popravki

--- lib/kodazakraj.R
+++ lib/kodazakraj.R
@@ -1,16 +1,5 @@
-#Rada bi kraj oz. ime maratona povezala z državo
-#To bom potem prekopirala v vizualizacijo.r
 
 
-# Kar se tiče države maratona, jo lahko ponekod dobiš iz
-# samega imena kraja (kjer je oblike Kraj, Država), 
-#drugod si pa lahko
-# pomagaš s pomožno tabelo (ali kar vektorjem), 
-#ki si ga lahko narediš v CSV
-# ali pa kar v R. Glede na to, da imaš le 24 različnih maratonov, tu ne bo
-# preveč dela.
-
-#kaj pa če bi naredila kar vekktor ,k i bi mi maratoen zamenjal z državami
 
 #številska spremenljivka..če se državljanstvo ujema z
 
@@ -25,25 +14,25 @@ kraj <- kraj[order(kraj, decreasing=TRUE)]
 kraj <- kraj[kraj %in% imenadrzav] #ali je kraj v imenu držav
 #če je, ga kar ?zamenjaj'?
 
-#to bo imenska spremenljivka, dodati moram še, 
-#da bo  tujec imel rdečo barvo, državljan modro, enako zeleno.
-#če dobim nek vektor
-# x = nam pove ali se država ujema z državljanstvom, y ,če se ne
-tujec <-  len(x) > len(y)...! %in% imenadrzav
-drzavljan <- len(y) > len(x)
-enako <- len(x) = len(y)
-tri <- c(tujec, drzavljan,enako)
-barve <- rainbow(3)
 
 
-
+drzave1 <- table(maraton$Drzava)
+domacin <- sapply(names(drzave1), function(x) sum(ujema[maraton$Drzava == x]))
+barve <- rep("white", nrow(svet))
+names(barve) <- svet$name_long
+tujec <- domacin < drzave1/2
+drzavljan <- domacin > drzave1/2
+enako <- domacin == drzave1/2
+barve[names(which(tujec))] <- "red"
+barve[names(which(drzavljan))] <- "blue"
+barve[names(which(enako))] <- "green"
 
 
 #točke na zemlejvidu, tukaj bi dala vektor z 
 #imeni maratonov? kako bo vedel, kje je kaj??
 # imena maratonov so v kraj :
-
-text(coordinates(svet), 
-     labels = as.character(kraj), cex = 0.7) #namesto krajev mi vrže številke
+plot(svet, col = barve)
+text(mesta, 
+     labels = as.character, cex = 0.7) #namesto krajev mi vrže številke
 barve <- rainbow(49)
 plot(svet, col = barve)
