78bf2e4873da8ec38893cf3c93d370c52292a5ee
Lea Tehovnik <TehovnikL12@R309-07.std.fmf.uni-lj.si>
2015-01-07 13:24:22 +0100

    Dodala nov stolpec že v prvo fazo.

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -41,4 +41,26 @@ maraton$Notes <- NULL
 maraton$Spol <- c(rep("M", 49), rep("Z",38))
 colnames(maraton) <- c("Čas", "Ime", "Drzavljanstvo", "Datum", "Kraj", "Spol")
 
+# Preuredimo podatke, da jih bomo lahko izrisali na zemljevid.
+kraji <- names(table(maraton$Kraj))
+maratoni <- gsub(".*, ", "", kraji)
+names(maratoni) <- kraji
+maratoni["Amsterdam Marathon"] <- "Netherlands"
+maratoni[grep("Beppu", maratoni)] <- "Japan"
+maratoni["Berlin Marathon"] <- "Germany"
+maratoni["Boston Marathon"] <- "United States"
+maratoni["Chicago Marathon"] <- "United States"
+maratoni["Dülmen"] <- "Germany"
+maratoni["Fukuoka Marathon"] <- "Japan"
+maratoni["London"] <- "United Kingdom"
+maratoni["New York City Marathon"] <- "United States"
+maratoni["Polytechnic Marathon"] <- "United Kingdom"
+maratoni["Seaside"] <- "United States"
+maratoni["Tour de Paris Marathon"] <- "France"
+maratoni["Ryde"] <- "United Kingdom"
+maratoni["Turku Marathon"] <- "Finland"
+maratoni["Yonkers"] <- "United States"
+maratoni["Yonkers,"] <- "United States"
+maratoni[grep("OR", maratoni)] <- "United States"
 
+maraton$Drzava <- maratoni[maraton$Kraj]
