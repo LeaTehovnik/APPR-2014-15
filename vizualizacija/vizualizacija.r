2afd50133c9039d6f3f4fd32aa458a0a15244af0
Lea Tehovnik <TehovnikL12@R309-07.std.fmf.uni-lj.si>
2015-01-07 12:54:25 +0100

    Urejanje

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -68,8 +68,8 @@ names(barve) <- names(rekordi)
 plot(svet, col = barve[as.character(svet$name_long)])
 title("Število podrtih maratonov")
 legend("left", legend = rekordiveni, fill = rgb(1, 0, 0, (1:length(rekordiveni))/length(rekordiveni)))
-imena <- c("London")
-mesta <- data.frame("long" = (52.52), "lat"= (-0.13))
+imena <- c("London", "Berlin", "Paris","Tokio")
+mesta <- data.frame("long" = c(51.51, 52.52, 48.85, 139.75), "lat"= c(-0.13, 13.41, 2.35,  35.68))
 text(coordinates(mesta[c("long", "lat")]),
      labels = imena,
      pos= 1, cex = 0.6, offset = 0.3)
@@ -161,6 +161,12 @@ n = 100
 #plot(obcine, col = barve)
 plot(svet, col = barve)
 title("Zmage domačinov in tujcev")
+legend("left", legend = c("Tujec", "Državljan", "Enako"), fill = c("red", "blue", "green"), cex = 0.6)
+imena <- c("London", "Berlin", "Paris","Tokio")
+mesta <- data.frame("long" = c(51.51, 52.52, 48.85, 139.75), "lat"= c(-0.13, 13.41, 2.35,  35.68))
+text(coordinates(mesta[c("long", "lat")]),
+     labels = imena,
+     pos= 1, cex = 0.6, offset = 0.3)
 
 
 dev.off()
