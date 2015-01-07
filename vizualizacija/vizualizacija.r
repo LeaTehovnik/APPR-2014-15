7f243891db1b6c93ca2337febaab43a66d941321
Lea Tehovnik <lea.tehovnik@gmail.com>
2015-01-07 19:31:41 +0100

    koordinate

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -72,9 +72,9 @@ title("Število podrtih maratonov")
 legend("left", legend = rekordiveni, fill = rgb(1, 0, 0, (1:length(rekordiveni))/length(rekordiveni)), cex = 0.5)
 imena <- c("London", "Berlin", "Paris","Tokio")
 mesta <- data.frame("long" = c(-0.13, 13.41, 2.35, 35.68), "lat"= c(51.51, 52.52, 48.85, 139.75))
-text(coordinates(mesta[c("long", "lat")]),
+text(coordinates(mesta),
      labels = imena,
-     pos= 1, cex = 0.6, offset = 0.3)
+     pos= 1, cex = 0.3, offset = 0.3)
 
 
 
@@ -162,9 +162,10 @@ title("Zmage domačinov in tujcev")
 legend("left", legend = c("Tujec", "Državljan", "Enako"), fill = c("red", "blue", "green"), cex = 0.6)
 imena <- c("London", "Berlin", "Paris","Tokio")
 mesta <- data.frame("long" = c(-0.13, 13.41, 2.35,  35.68), "lat"= c(51.51, 52.52, 48.85, 139.75))
-text(coordinates(mesta[c("long", "lat")]),
+text(coordinates(mesta),
      labels = imena,
      pos= 1, cex = 0.6, offset = 0.3)
+
 
 
 dev.off()
