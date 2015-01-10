60e2a8f82cae70ec391043e68736cd4a367b2be2
Lea Tehovnik <lea.tehovnik@gmail.com>
2015-01-10 22:52:45 +0100

    delam 4. fazo

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -3,15 +3,27 @@
 # Uvozimo funkcijo za uvoz spletne strani.
 source("lib/xml.r")
 
-# Preberemo spletno stran v razpredelnico.
-cat("Uvažam spletno stran...\n")
-tabela <- preuredi(uvozi.obcine(), obcine)
+# # Preberemo spletno stran v razpredelnico.
+# cat("Uvažam spletno stran...\n")
+# tabela <- preuredi(uvozi.obcine(), obcine)
+# 
+# # Narišemo graf v datoteko PDF.
+# cat("Rišem graf...\n")
+# pdf("slike/naselja.pdf", width=6, height=4)
+# plot(tabela[[1]], tabela[[4]],
+#      main = "Število naselij glede na površino občine",
+#      xlab = "Površina (km^2)",
+#      ylab = "Št. naselij")
+# dev.off()
 
-# Narišemo graf v datoteko PDF.
-cat("Rišem graf...\n")
-pdf("slike/naselja.pdf", width=6, height=4)
-plot(tabela[[1]], tabela[[4]],
-     main = "Število naselij glede na površino občine",
-     xlab = "Površina (km^2)",
-     ylab = "Št. naselij")
+pdf("slike/graf4.pdf")
+pdf.options(encoding='ISOLatin2.enc')
+
+cas <- maraton$Čas
+datum <- names(table(maraton$Datum))
+leto <- gsub(".*, ", "", datum)
+names(leto) <- datum
+plot(leto, cas, main = "Spreminjanje rekorda skozi čas", 
+     xlab = "Letnica maratona", ylab = "Čas v sekundah", type = "h", lwd = 5, col = "steelblue")
+
 dev.off()
