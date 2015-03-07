8eedfa68af8a3dcc40e1dba6d2ea3361b2027cdd
Lea Tehovnik <lea.tehovnik@gmail.com>
2015-03-07 16:14:58 +0100

    animacija

--- analiza/animacija.R
+++ analiza/animacija.R
@@ -32,7 +32,7 @@ preuredi <- function(podatki, zemljevid) {
 
 # Narišimo zemljevid v PDF.
 cat("Rišem zemljevid...\n")
-mypdf("slike/ani1.pdf", width = 7.27, height = 5.69, family = "Arial") 
+mypdf("porocilo/ani.pdf", width = 8, height = 6, family = "Arial") 
 
 
 
@@ -43,18 +43,74 @@ mypdf("slike/ani1.pdf", width = 7.27, height = 5.69, family = "Arial")
 # barve <- rgb(1, 0, 0, match(rekordi, rekordiveni)/length(rekordiveni))
 # names(barve) <- names(rekordi)
 #plot(svet, col = barve[as.character(svet$name_long)])
-plot(svet)
-title("Rekord")
+# plot(svet)
+# title("Rekord")
 #legend("left", legend = rekordiveni, fill = rgb(1, 0, 0, (1:length(rekordiveni))/length(rekordiveni)), cex = 0.5)
 #imena <- c("London", "New York", "Tokyo")
 #mesta <- data.frame("long" = c(-0.13, -74.01, 139.75), "lat"= c(51.51, 40.71,35.68))
-text(coordinates(mesta),
-     labels = imena,
-     pos = c(2,4,1), cex = 0.6,)
-points(coordinates(mesta), type = "p", pch = 15, cex = 0.4, col = "green")
+
+
 
 
 plot(svet)
-title("Rekord1")
+title("Rekordi leta 1908")
+#imena <- c("London")
+mesta <- data.frame("long" = c(-0.13), "lat" = c(51.51))
+# text(coordinates(mesta),
+#      labels = imena,
+#      pos = c(2), cex = 0.6,)
+points(coordinates(mesta), type = "p", pch = 19, cex = 1, col = "steelblue")
+
+plot(svet)
+title("Rekordi leta 1909")
+#imena <- c("London", "New York")
+mesta <- data.frame("long" = c(-0.13, -74.01, 18.05), "lat" = c(51.51, 40.71,59.33))
+# text(coordinates(mesta),
+#      labels = imena,
+#      pos = c(2), cex = 0.6,)
+points(coordinates(mesta), type = "p", pch = 19, cex = c(1, 3, 1), col = "steelblue")
+
+plot(svet)
+title("Rekordi leta 1913")
+mesta <- data.frame("long" = c(-0.13), "lat" = c(51.51))
+points(coordinates(mesta), type = "p", pch = 19, cex = 2, col = "steelblue")
+
+plot(svet)
+title("Rekordi leta 1914")
+mesta <- data.frame("long" = c(8.91), "lat" = c(45.60))
+points(coordinates(mesta), type = "p", pch = 19, cex = 1, col = "steelblue")
+                    
+plot(svet)
+title("Rekordi leta 1918")
+mesta <- data.frame("long" = c(2.35), "lat" = c(48.86))
+points(coordinates(mesta), type = "p", pch = 19, cex = 1, col = "pink")
+                    
+
+plot(svet)
+title("Rekordi leta 1920")
+mesta <- data.frame("long" = c(4.40), "lat" = c(51.21))
+points(coordinates(mesta), type = "p", pch = 19, cex = 1, col = "steelblue")
+                    
+plot(svet)
+title("Rekordi leta 1925")
+mesta <- data.frame("long" = c(-74.01), "lat" = c(40.71))
+points(coordinates(mesta), type = "p", pch = 19, cex = 1, col = "steelblue")                    
+
+plot(svet)
+title("Rekordi leta 1926")
+mesta <- data.frame("long" = c(-0.13), "lat" = c(51.51))
+points(coordinates(mesta), type = "p", pch = 19, cex = 1, col = "pink")
+
+plot(svet)
+title("Rekordi leta 1929")
+mesta <- data.frame("long" = c(-0.13), "lat" = c(51.51))
+points(coordinates(mesta), type = "p", pch = 19, cex = 1, col = "steelblue")
+
+plot(svet)
+title("Rekordi leta 1935")
+mesta <- data.frame("long" = c(139.69), "lat" = c(35.69))
+points(coordinates(mesta), type = "p", pch = 19, cex = 4, col = "steelblue")
+
+
 
 dev.off()
