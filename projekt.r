35141787bacf095c29538e6a34488b302d07f1ac
Lea Tehovnik <lea.tehovnik@gmail.com>
2015-03-11 17:33:27 +0100

    dodala še drugo animacijo, jjutri na faksu še šumniki

--- projekt.r
+++ projekt.r
@@ -18,5 +18,6 @@ source("vizualizacija/vizualizacija.r",encoding="UTF-8")
 # 4. faza: Napredna analiza podatkov
 source("analiza/analiza.r",encoding="UTF-8")
 source("analiza/animacija.R",encoding="UTF-8")
+source("analiza/rekordikraj.R",encoding="UTF-8")
 
 cat("Končano.\n")
